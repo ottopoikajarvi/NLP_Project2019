0d5f2cf3556f2f03e888ff2d81ed97d3f79b4795
Jii <10657836+Jiiiiiiiii@users.noreply.github.com>
2020-01-07 17:43:47 +0200

    Add files via upload

---
+++ plotSentenceLength.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+
+def plotSentenceLength(sentences):
+    # Results?
+    sentDF=pd.DataFrame(sentences)
+    sentDF.columns=['date','year','month','raw','length','lower','words','stemmed']
+    meansDF=sentDF.groupby(['date']).mean()
+    meansDF['date']=meansDF.index
+    meansDF.index.name='index'
+
+    mindate=meansDF['date'].min()
+
+    emptyDF=pd.DataFrame({'date':pd.date_range("{:02d}".format(int(mindate[5:6]))+'-01-'+mindate[0:4],'12-31-2018',freq='M').strftime('%Y%m')})
+
+    meansDF=pd.merge(meansDF,emptyDF,how='outer', on=['date'])
+
+    meansDF.sort_values(by=['date']).plot.bar(x='date',y='length',figsize = (13.5, 9))
